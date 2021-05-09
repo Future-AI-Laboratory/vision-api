0d5f20e200adc3de94560df027d5abe64ac19653
SAYAN HAZRA <61377635+sayan0506@users.noreply.github.com>
2021-05-09 21:34:16 +0530

    Add files via upload

--- Utilities/data_analysis.py
+++ Utilities/data_analysis.py
@@ -2,24 +2,69 @@
 It analyzes the content of the dataset of particular crop-type
 (This code block is optional, while running the ML pipeline)
 
-Note: This comparison does not cover or include the original vs augmented comparison
-Todo : Standardising and lint 
-
+Note: 1. This comparison does not cover or include the original vs augmented comparison
+2. We dynamically store images of all the classes of particular type to a seperate image folder
+3. We are not using image_data_generator.flow_from_directory(), as we need to do startified split
+4. Store the image names to a dataframe, and image name should be a dynamic name while copying from source dataset folder
+5. Issue with the dynamic name is it copies to a destination folder to a seperate name(Ex. source(118,119,120,121) to dst(118, 119, 12, 121)
+6. To avoid that count the total images of that croptype present in the dataset for all classes
+7. Count number of digits(ex. total 3000 images, digit count = 4)
+8. Then, use integer precision to ensure the destination name as (0,1,2,3...) as (0000, 0001, 0002....,0100, ....2999)
+9. To do integer precision use dynamic string (str = '{:'+str(digit_count)+'d}') (Ex: '{:4d}.jpg'.format(sample_count))
 '''
+
 import os
 import numpy as np
 from matplotlib import pyplot as plt
 import pandas as pd
 from keras.preprocessing.image import img_to_array
 import seaborn as sns
+import shutil
 
 # defining the data inspection class
 class data_inspection:
-  def __init__(self, dataset_folder, classifiers_list = None, crop_type = None):
+  def __init__(self, dataset_folder, image_folder_path = 'images', classifiers_list = None, crop_type = None):
+    '''
+    For simplicity of flow_from_dataframe function
+    After fetching each image info, the image is stored to a single folder
+    'image_folder_path' defines the signle path
+    '''
+    # the dataset path (here master path of plant_village_dataset)
     self.dataset_path = dataset_folder
+    # classifiers list(specifically classifiers folder)
     self.classifiers = classifiers_list
+    # crop type
     self.crop_type = crop_type
+    # destination image folder where images of particular crop type(for all classes) will be stored together
+    # for the purpose of imagedatagenerator
+    self.image_folder = image_folder_path
     
+    if not os.path.isdir(self.image_folder):
+      os.mkdir(self.image_folder)
+      print(f'The main image folder "{self.image_folder}" is created for {self.crop_type}')
+
+    else:
+      print(f'"{self.image_folder}" is already present to copy the images as a whole!') 
+    
+    # calls sample_image_digit_count to return total 
+    digit_count = self.sample_image_count()
+    self.digit_count_str = '{:'+str(digit_count)+'d}.jpg'
+    print(f'All the image names in the destination folder will be {digit_count} digit numbers!')
+     
+
+  def sample_image_count(self):
+    '''
+    Caounts total image samples of that crop_type correspondin to all the individual class
+    '''
+    # counts total image samples
+    class_img_sample_count = 0
+    for cls in self.classifiers:
+      cls_path = os.path.join(self.dataset_path, cls)
+      class_img_sample_count += len(os.listdir(cls_path))
+
+    # length of total digits in the image samples count
+    return len(str(class_img_sample_count))  
+
   def dataset_content(self):
     # checks the list of all disease classes available in the dataset/image folder
     disease_classes = os.listdir(self.dataset_path)
@@ -47,10 +92,13 @@ class data_inspection:
   def fetch_img_info(self):
     '''
     Fetch the distribution of different classifiers and shapes
+    While fetching individual image info, copy them to a single folder 'images'
+    It will help to use imagedatagenerator.flow_from_dataframe()
+
     '''
 
     crop_classifiers_path = [os.path.join(self.dataset_path, disease) for disease in self.classifiers]
-    print(f'{self.crop_type} directory names list {crop_classifiers_path}')
+    print(f'{self.crop_type.upper()} directory names list {crop_classifiers_path}')
     
     # image array list
     #image_list = []
@@ -81,12 +129,20 @@ class data_inspection:
           disease_sample_path = os.path.join(crop_disease_path, disease_sample)
           if disease_sample_path.endswith(".jpg") == True or disease_sample_path.endswith(".JPG") == True:
             img = self.convert_img_to_array(disease_sample_path)
+            # copy the image to 'images' folder (self.image_folder)
+            # sample destination path of crop image(sample_dst_path_ci)
+            #str_path = 
+            sample_dst_path_ci = self.digit_count_str.format(sample_count)
+            sample_dest_path = os.path.join(self.image_folder, sample_dst_path_ci)
+            #print(sample_dest_path)
+            shutil.copy(disease_sample_path, sample_dest_path)
             #image_list.append(img)
             #label_list.append(potato_classifiers[sr])
-            self.img_df.loc[sample_count,'Image'], self.img_df.loc[sample_count,'Label'] = disease_sample, self.classifiers[sr]
+            self.img_df.loc[sample_count,'Image'], self.img_df.loc[sample_count,'Label'] = sample_dst_path_ci, self.classifiers[sr]
             self.img_df.loc[sample_count,'Shape'] = img.shape
             count += 1
             sample_count += 1
+
         self.label_df.loc[0, self.classifiers[sr]] = count
       
       self.label_shape_df = self.img_df[['Shape', 'Label']].value_counts()
@@ -95,7 +151,11 @@ class data_inspection:
       self.shape_imbalance = self.label_shape_df.shape[0] > len(self.classifiers)
       
       # image_list, label_list, 
-
+      
+      # checks whether all the images are copied to single image folder or not
+      assert len(os.listdir(self.image_folder)) == sample_count
+      print(f'All the {sample_count} images for {self.crop_type} are copied to image folder successfully, test passed!')
+# 
       return self.label_df, self.img_df, self.shape_imbalance   
         
     except Exception as e:
